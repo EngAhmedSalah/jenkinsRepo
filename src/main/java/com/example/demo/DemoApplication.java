fc9f8de839880b97578684d3210b6801b6542a07
EngAhmedSalah <ahmed.salahfci1164@gmail.com>
2021-10-01 17:08:33 +0200

    code updated to test jenkins #2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Ahmed Salah testing jenkins 18-04-1998");
+		System.out.println("Ahmed Salah testing jenkins 18-04-1998555");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
